1e50063cda255b6e477b0aebe78c220373ababf6
nttung42 <ntt.a5k62.cbh@gmail.com>
2024-05-31 02:51:27 +0700

    my first react app

---
+++ myapp/src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>My second React app</h1>
+        <p>Welcome to my page</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
