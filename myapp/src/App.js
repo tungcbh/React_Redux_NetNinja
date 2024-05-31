f9b4a6e92308c99eae24663cb6699394b1b98514
nttung42 <ntt.a5k62.cbh@gmail.com>
2024-05-31 12:27:05 +0700

    condition ouput

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import FirstComponent from './FirstComponent';
 class App extends Component {
+  state = {
+    Data : [
+      {name : 'tung', age: 29, uni:"fpt", id:1},
+      {name : 'vu', age: 18, uni:"hust", id:2},
+      {name : 'phg', age: 20, uni:"uet", id:3},
+
+    ]
+  }
   render() {
     return (
       <div className="app-container">
         <h1>My second React app</h1>
         <p>Welcome to my page</p>
-        <FirstComponent />
+        <FirstComponent Data={this.state.Data}/>
         {/* <firstComponent name="tung" age="25" uni="FBT" /> */}
       </div>
     );
--- myapp/src/FirstComponent.js
+++ myapp/src/FirstComponent.js
@@ -1,16 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class FirstComponent extends Component {
-    render() {
-        const {name, age, uni} = this.props;
+const FirstComponent = ({ Data }) => {
+    // const List = Data.map(item => {
+    //     if (item.age >= 20) {
+    //         return (
+    //             <div className="tung" key={item.id}>
+    //                 <div>Name: {item.name}</div>
+    //                 <div>Age:{item.age}</div>
+    //                 <div>University: {item.uni}</div>
+    //             </div>
+    //         )
+    //     }
+    // })
 
-        return (
-            <div className="tung">
-                <div>Name: {name}</div>
-                <div>Age:{age}</div>
-                <div>University: {uni}</div>
+    const List = Data.map(item => {
+        return (item.age >= 20) ? (
+            <div className="tung" key={item.id}>
+                <div>Name: {item.name}</div>
+                <div>Age:{item.age}</div>
+                <div>University: {item.uni}</div>
             </div>
-        );
-    }
+        ) : null;
+    })
+
+    return (
+        <div className='item-list'>
+            {List}
+        </div>
+    )
+
 }
 export default FirstComponent
