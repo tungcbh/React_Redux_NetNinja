34231fba50bd295c7fe19e7af4c69d30515adeef
nttung42 <ntt.a5k62.cbh@gmail.com>
2024-06-01 00:16:50 +0700

    router

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,43 +1,25 @@
 import React, { Component } from 'react';
-import Todos from './Todos';
-import AddTodo from './AddForm';
-class App extends Component{
-  state = {
-    todos: [
-      {id: 1, content: 'buy some vege'},
-      {id: 2, content: 'learn code'}
-    ]
-  }
+// import {Router, Route  } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-  addTodo = (todo) => {
-    todo.id = Number(Math.random());
-    let todos = [...this.state.todos, todo];
-    this.setState({
-      todos: todos
-    })
-
-  }
-
-  deleteTodo = (id) => {
-    console.log(id);
-    const todos = this.state.todos.filter(todo => {
-      return todo.id !== id;
-    })
-
-    this.setState({
-      todos: todos
-    })
-  }
- 
-
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+class App extends Component {
   render() {
     return (
-      <div className="todo-app container">
-        <h1 className='center blue-text'>Todo's</h1>
-        
-        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
-        <AddTodo addTodo={this.addTodo} />
-      </div>
+
+      <BrowserRouter>
+        <div className='App'>
+          <Navbar />
+          <Route path="/home" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+        </div>
+      </BrowserRouter>
+
+
     );
   }
 
--- myapp/src/components/Navbar.js
+++ myapp/src/components/Navbar.js
@@ -4,9 +4,9 @@ const Navbar = () => {
     return (
         <nav className="nav-wrapper red darken-3">
             <div className="container">
-                <a  href="/" className="brand-logo">Lgo</a>
+                <a href="/"  className="brand-logo">Logo</a>
                 <ul className="right">
-                    <li><a href="/"> Home </a></li>
+                    <li><a href="/home"> Home </a></li>
                     <li><a href="/about"> About </a></li>
                     <li><a href="/contact"> Contact </a></li>
                 </ul>
